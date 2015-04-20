e6c8fc8ebbcb305becb3d779fbf0ef0c9bb3f6a4
mtrasobaresb <mgavinbel@gmail.com>
2015-04-21 00:58:50 +0200

    Añadida activity Información de un Producto.
    - Se ha añadido una nueva activity "InfoProducto" que muestra la
    información de un producto. También permite lanzar la activity de
    "EnvioPedido". El campo relativo al SO queda como número
    provisionalmente.
    
    - También se ha modificado el xml "info_producto" relativo a la
    activity, estableciendo correctamente sus ids y valores constantes.

--- src/com/zaratech/smarket/aplicacion/InfoProducto.java
+++ src/com/zaratech/smarket/aplicacion/InfoProducto.java
@@ -1,14 +1,30 @@
 package com.zaratech.smarket.aplicacion;
 
-import com.zaratech.smarket.componentes.Producto;
 import com.zaratech.smarket.R;
+import com.zaratech.smarket.componentes.Producto;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.app.Activity;
+import android.content.Intent;
 
 public class InfoProducto extends Activity {
 
+	private ImageView imagen;
+	private TextView nombreText;
+    private TextView descripcionText;
+    private TextView marcaText;
+    private TextView pantallaText;
+    private TextView sistemaOpText;
+    private TextView precioText;
+    private Button comprarButt;
+	
+    private Producto p;
+    
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		
@@ -16,9 +32,41 @@ public class InfoProducto extends Activity {
 		setContentView(R.layout.activity_info_producto);
 		
 		/* Prueba de recibir Producto */
-		Producto p = this.getIntent().getExtras().getParcelable("Producto");
+		p = this.getIntent().getExtras().getParcelable("Producto");
 		Toast.makeText(this, p.getNombre(), Toast.LENGTH_LONG).show();
 		/* ************************** */
-	}
+        //setContentView(R.layout.note_edit);
+        //setTitle(R.string.edit_note);
+		imagen = (ImageView) findViewById(R.id.image);
+	    nombreText = (TextView) findViewById(R.id.name_text);
+	    descripcionText = (TextView) findViewById(R.id.description_text);
+	    marcaText = (TextView) findViewById(R.id.brand_text);
+	    pantallaText = (TextView) findViewById(R.id.screen_text);
+	    sistemaOpText = (TextView) findViewById(R.id.operative_system_text);
+	    precioText = (TextView) findViewById(R.id.price_text);
+	    comprarButt = (Button) findViewById(R.id.buy_button);
+	    
+	    // Rellenado de campos
+	    imagen.setImageBitmap(p.getImagen());
+	    nombreText.setText (p.getNombre());
+	    String DESCRIPCION_PRUEBA = "Este telefono es muy bueno, el mejor del mercado en calidad/precio";
+	    //descripcionText.setText (p.getDescripcion());
+	    descripcionText.setText (DESCRIPCION_PRUEBA);
+	    marcaText.setText (p.getMarca().getNombre());
+	    pantallaText.setText (String.format("%.2f %s", p.getDimensionPantalla(), getString(R.string.UINCH)));
+	    sistemaOpText.setText (String.format("%d", p.getSistemaOperativo()));
+	    precioText.setText (String.format("%.2f %s", p.getPrecio(), getString(R.string.UM)));
+	    
+		comprarButt.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+            	Intent i = new Intent(InfoProducto.this, EnvioPedido.class);
+            	i.putExtra("source", "noteEdit");
+        		
+        		// Añade Producto recibido
+        		i.putExtra("Producto", p);
 
+        		startActivity(i);
+            }
+        });
+	}
 }

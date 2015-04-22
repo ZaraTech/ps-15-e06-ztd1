f09f3928df492eb6345f3843dae0e200b32138cc
mtrasobaresb <mgavinbel@gmail.com>
2015-04-22 11:56:12 +0200

    Mejoras interfaz de activity InfoProducto
    - Campos correctamente alineados.
    - Añadido el campo "Sistema operativo" correctamente.
    - Añadido un campo dinámico para las ofertas, mostrando el precio
    anterior tachado.

--- src/com/zaratech/smarket/aplicacion/InfoProducto.java
+++ src/com/zaratech/smarket/aplicacion/InfoProducto.java
@@ -2,15 +2,19 @@ package com.zaratech.smarket.aplicacion;
 
 import com.zaratech.smarket.R;
 import com.zaratech.smarket.componentes.Producto;
+import com.zaratech.smarket.utiles.AdaptadorBD;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Paint;
 
 public class InfoProducto extends Activity {
 
@@ -21,6 +25,7 @@ public class InfoProducto extends Activity {
     private TextView pantallaText;
     private TextView sistemaOpText;
     private TextView precioText;
+    private TextView precioOfertaText;
     private Button comprarButt;
 	
     private Producto p;
@@ -44,18 +49,26 @@ public class InfoProducto extends Activity {
 	    pantallaText = (TextView) findViewById(R.id.screen_text);
 	    sistemaOpText = (TextView) findViewById(R.id.operative_system_text);
 	    precioText = (TextView) findViewById(R.id.price_text);
+	    precioOfertaText = (TextView) findViewById(R.id.price_ofert_text);
 	    comprarButt = (Button) findViewById(R.id.buy_button);
 	    
 	    // Rellenado de campos
 	    imagen.setImageBitmap(p.getImagen());
 	    nombreText.setText (p.getNombre());
-	    String DESCRIPCION_PRUEBA = "Este telefono es muy bueno, el mejor del mercado en calidad/precio";
+	    String descripcion_prueba = "Este teléfono es muy bueno, el mejor del mercado en calidad/precio";
 	    //descripcionText.setText (p.getDescripcion());
-	    descripcionText.setText (DESCRIPCION_PRUEBA);
+	    descripcionText.setText (descripcion_prueba);
 	    marcaText.setText (p.getMarca().getNombre());
 	    pantallaText.setText (String.format("%.2f %s", p.getDimensionPantalla(), getString(R.string.UINCH)));
-	    sistemaOpText.setText (String.format("%d", p.getSistemaOperativo()));
+	    sistemaOpText.setText (AdaptadorBD.obtenerSistemaOperativo(p.getSistemaOperativo()));
 	    precioText.setText (String.format("%.2f %s", p.getPrecio(), getString(R.string.ud_monetaria)));
+	    
+	    if (p.isOferta()) {
+	    	precioText.setPaintFlags(precioText.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+	    	precioText.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT, 0.8f));
+	    	precioOfertaText.setText(String.format("%.2f %s", p.getPrecioOferta(), getString(R.string.ud_monetaria)));
+	    	precioOfertaText.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT, 1f));
+	    }
 	    
 		comprarButt.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {

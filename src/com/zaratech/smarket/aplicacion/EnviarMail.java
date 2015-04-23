295b06d08316e76b92e6062f021cc5f91c1d2eea
MartaFr <mfriaszapater@gmail.com>
2015-04-23 20:36:13 +0200

    Implementacion del envio de la orden de compra - Actualizacion de la interfaz de envio - Implementacion del envio en tarea en segundo plano - Añadidas strings al fichero strings.xml

---
+++ src/com/zaratech/smarket/aplicacion/EnviarMail.java
@@ -0,0 +1,162 @@
+package com.zaratech.smarket.aplicacion;
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import com.zaratech.smarket.R;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.util.Log;
+
+public class EnviarMail extends AsyncTask<String, Object, Object> {
+
+	/**
+	 * Texto que muestra el estado del envio en la aplicación
+	 */
+	private ProgressDialog estado;
+
+	/**
+	 * Activity que realiza la peticion de envio.
+	 */
+	private Activity enviarMailActivity;
+	
+	/*
+	 * Datos a incluir en el correo
+	 */
+	private final String DESTINATARIO = "make_the_king@hotmail.com";
+	private final String ASUNTO_MSJ = "[SMARKET] Orden de pedido - ";
+	private final String TEXTO_MSJ = "Texto de la orden de pedido...";
+
+	/*
+	 * Datos de la cuenta con la que se enviara el correo
+	 */
+	private final String NOMBRE_USUARIO = "smarket.zaratech@gmail.com";
+	private final String PASS = "SmarketZT1506";
+	private final String DIRECCION_ORIGEN = NOMBRE_USUARIO;
+
+	/*
+	 * Control del envio
+	 */
+	private final String ENVIO_OK = "OK";
+	private final String ENVIO_KO = "KO";
+
+	/**
+	 * Propiedades asociadas al envio de correo electronico
+	 */
+	private Properties props = new Properties();
+
+	/**
+	 * Constructor
+	 * @param activity Activity que realiza la peticion de envio.
+	 */
+	public EnviarMail(Activity activity) {
+		enviarMailActivity = activity;
+		
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+
+	}
+
+	@Override
+	protected void onPreExecute() {
+
+		// Crea el cuadro de texto que mostrara el estado durante el envio
+		estado = new ProgressDialog(enviarMailActivity);
+		estado.setMessage(enviarMailActivity.getString(R.string.pedido_preparar));
+		estado.setIndeterminate(false);
+		estado.setCancelable(false);
+		estado.show();
+	}
+
+	@Override
+	public void onProgressUpdate(Object... values) {
+		estado.setMessage(values[0].toString());
+
+	}
+	
+	@Override
+	public void onPostExecute(Object result) {
+		estado.dismiss();
+		
+		AlertDialog.Builder b = new AlertDialog.Builder(enviarMailActivity);
+		String resultado = (String)result;
+		
+		if (resultado.compareTo(ENVIO_OK) == 0) {
+
+			b.setTitle(enviarMailActivity
+					.getString(R.string.pedido_envio_exito));
+			b.setMessage(String.format("\n%s\n",
+					enviarMailActivity.getString(R.string.pedido_enviado)));
+			b.setNeutralButton(
+					enviarMailActivity.getString(R.string.boton_aceptar),
+					new DialogInterface.OnClickListener() {
+
+						public void onClick(DialogInterface d, int which) {
+
+							Intent i = new Intent(enviarMailActivity
+									.getBaseContext(), ListaProductos.class);
+							enviarMailActivity.startActivity(i);
+						}
+					});
+
+		} else {
+
+			b.setTitle(enviarMailActivity
+					.getString(R.string.pedido_envio_error));
+			b.setMessage(String.format("\n%s\n",
+					enviarMailActivity.getString(R.string.pedido_no_enviado)));
+			b.setNeutralButton(
+					enviarMailActivity.getString(R.string.boton_aceptar),
+					new DialogInterface.OnClickListener() {
+
+						public void onClick(DialogInterface d, int which) {
+							// si error no vuelve al listado
+						}
+					});
+
+		}
+		
+		AlertDialog a = b.create();
+		a.show();
+	}
+	
+	protected String doInBackground(String... idPedidoCliente) {
+		try {			
+			Session s = Session.getInstance(props);
+			Transport t = s.getTransport("smtp");
+			Message message = new MimeMessage(s);
+
+			message.setFrom(new InternetAddress(DIRECCION_ORIGEN));
+			message.setContent(TEXTO_MSJ, "text/html");
+			message.setRecipients(MimeMessage.RecipientType.TO,
+					InternetAddress.parse(DESTINATARIO));
+			message.setSubject(ASUNTO_MSJ + " " + idPedidoCliente[0]);
+
+			t.connect(NOMBRE_USUARIO, PASS);
+			publishProgress(enviarMailActivity.getString(R.string.pedido_enviar));
+			t.sendMessage(message, message.getAllRecipients());	
+
+			Log.i("Smarket_EnviarMail", "Correo enviado!");
+			
+			return ENVIO_OK;
+
+		} catch (Exception e) {
+			Log.e("Smarket_EnviarMail_Exception", e.getMessage());
+			
+			return ENVIO_KO;
+		}
+	}
+
+}
--- src/com/zaratech/smarket/aplicacion/EnvioPedido.java
+++ src/com/zaratech/smarket/aplicacion/EnvioPedido.java
@@ -1,17 +1,94 @@
 package com.zaratech.smarket.aplicacion;
 
 import com.zaratech.smarket.R;
+import com.zaratech.smarket.componentes.Producto;
+import com.zaratech.smarket.utiles.AdaptadorBD;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.app.Activity;
+import android.graphics.Paint;
 
 public class EnvioPedido extends Activity {
-
+	
+	/*
+	 * Variables asociadas a los campos del layout
+	 */
+	private TextView mNombreProducto;
+	private TextView mMarcaProducto;
+	private TextView mPrecioProducto;
+	private TextView mPrecioOfertaProducto;
+	private TextView mTipoProducto;
+	private TextView mSistemaOperativo;
+	private EditText mIdPedidoCliente;
+	private ImageView mImagenProducto;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_envio_pedido);
-	}
+		
+		Producto pPrueba = getIntent().getExtras().getParcelable("Producto");
+		
+		mNombreProducto = (TextView)findViewById(R.id.Nombre);
+		mMarcaProducto = (TextView)findViewById(R.id.Marca);
+		mTipoProducto = (TextView)findViewById(R.id.Tipo);
+		mSistemaOperativo = (TextView)findViewById(R.id.Sistema_operativo);
+		mPrecioProducto = (TextView)findViewById(R.id.Precio);
+		mPrecioOfertaProducto = (TextView)findViewById(R.id.Precio_oferta);
+		mIdPedidoCliente = (EditText)findViewById(R.id.PedidoCliente);
+		mImagenProducto = (ImageView) findViewById(R.id.Imagen);
+		
+		Button botonEnviar = (Button) findViewById(R.id.Enviar);
+		
+		mNombreProducto.setText(pPrueba.getNombre());
+		mMarcaProducto.setText(pPrueba.getMarca().getNombre());
+		mTipoProducto.setText(AdaptadorBD.obtenerTipo(pPrueba.getTipo()));
+		mSistemaOperativo.setText(AdaptadorBD.obtenerSistemaOperativo(
+				pPrueba.getSistemaOperativo()));
+		mPrecioProducto.setText(String.format("%.2f %s", pPrueba.getPrecio(),
+				getString(R.string.ud_monetaria)));
+		
+		if (pPrueba.isOferta()) {
+			mPrecioProducto.setPaintFlags(mPrecioProducto.getPaintFlags()
+					| Paint.STRIKE_THRU_TEXT_FLAG);
 
+			mPrecioOfertaProducto.setText(String.format("%.2f %s",
+							pPrueba.getPrecioOferta(),
+							getString(R.string.ud_monetaria)));
+		}
+		
+		if(pPrueba.getImagen() != null){
+			mImagenProducto.setImageBitmap(pPrueba.getImagen());
+		}	
+		
+		
+		botonEnviar.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View view) {
+				
+				// Enviar email
+				String idPedido = mIdPedidoCliente.getText().toString();
+
+				if (!idPedido.isEmpty()) {
+					
+					// Ejecuta el envio en 2o plano
+					EnviarMail em = new EnviarMail(EnvioPedido.this);
+					em.execute(idPedido);
+					
+					
+				} else {
+					Toast.makeText(getBaseContext(), R.string.pedido_envio_error,
+							Toast.LENGTH_SHORT).show();
+				}
+			}
+		});
+		
+	}
 }
